d06bb802cb518a4d4f0db275371f2d3b291a93e0
jada <67612300+marqidox@users.noreply.github.com>
2024-01-28 16:56:09 -0600

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -81,37 +81,37 @@ def callback(frame):
     img = frame.to_ndarray(format="bgr24")
     student = Student()
     teacher = Teacher()
-    if toggle.is_on():  
-        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-            img.flags.writeable = False
-            results = holistic.process(img)
-            img.flags.writeable = True  # prevents copying the image data, we're able to use it for rendering
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            # pose
-            pose = results.pose_landmarks.landmark
-            pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
-            # face
-            face = results.face_landmarks.landmark
-            face_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
-            # can eventually combine the hand landmarks too
-            # combining them into one big array
-            row = pose_row + face_row
-            x = pd.DataFrame([row])
-            body_language_class = model.predict(x)[0]  # first value of the predict array
-            teacher.append_data(body_language_class.lower())
-            msg = new_student.update_emotion(body_language_class.lower())
-            y_max = int(max([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
-            y_min = int(min([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
-            x_max = int(max([landmark.x for landmark in results.face_landmarks.landmark]) * 640)
-            x_min = int(min([landmark.x for landmark in results.face_landmarks.landmark]) * 640)
-            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255, 255, 255), 2)
-            cv2.rectangle(img, (x_min, y_min - 30), (x_max + len(body_language_class), y_min), (245, 117, 16), -1)
-            cv2.putText(img, body_language_class, (x_min + 2, y_min - 4), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
-                        cv2.LINE_AA)
-            cv2.rectangle(img, (0, 0), (0 + len(msg) * 15 + 7, 35), (0, 0, 0), -1)
-            cv2.rectangle(img, (2, 2), (0 + len(msg) * 15 + 5, 33), (255, 255, 255), -1)
-            cv2.putText(img, msg, (9, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+    #if toggle.is_on():  
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img.flags.writeable = False
+        results = holistic.process(img)
+        img.flags.writeable = True  # prevents copying the image data, we're able to use it for rendering
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # pose
+        pose = results.pose_landmarks.landmark
+        pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+        # face
+        face = results.face_landmarks.landmark
+        face_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+        # can eventually combine the hand landmarks too
+        # combining them into one big array
+        row = pose_row + face_row
+        x = pd.DataFrame([row])
+        body_language_class = model.predict(x)[0]  # first value of the predict array
+        #teacher.append_data(body_language_class.lower())
+        msg = new_student.update_emotion(body_language_class.lower())
+        y_max = int(max([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
+        y_min = int(min([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
+        x_max = int(max([landmark.x for landmark in results.face_landmarks.landmark]) * 640)
+        x_min = int(min([landmark.x for landmark in results.face_landmarks.landmark]) * 640)
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255, 255, 255), 2)
+        cv2.rectangle(img, (x_min, y_min - 30), (x_max + len(body_language_class), y_min), (245, 117, 16), -1)
+        cv2.putText(img, body_language_class, (x_min + 2, y_min - 4), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+        cv2.rectangle(img, (0, 0), (0 + len(msg) * 15 + 7, 35), (0, 0, 0), -1)
+        cv2.rectangle(img, (2, 2), (0 + len(msg) * 15 + 5, 33), (255, 255, 255), -1)
+        cv2.putText(img, msg, (9, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
