68e6eb35c8a6179d8fb8a35b6ee801cef0024e69
jada <67612300+marqidox@users.noreply.github.com>
2024-02-03 15:46:40 -0600

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -44,7 +44,7 @@ class Student:
 st.title("Body Language Detection for Online Learning")
 st.header("Student Interface")
 #st.toggle("Activate Body Language Detector")
-with open(r"body_language_model_official_rfc3.pkl","rb") as f:
+with open(r"body_language_model_official_gbc3.pkl","rb") as f:
     model = pickle.load(f)
 
 def callback(frame):
